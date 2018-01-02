7fe685bf51ce76c7b81a1b78fc9690bf7d6d9398
PuckmanXY <kayoanderson0403@gmail.com>
2018-01-02 04:36:35 -0300

    Now my portfolio uses the VueJS framework, AWESOME!

---
+++ js/vue.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        sections : [
+            {
+                name: 'Sobre',
+                href: '#About',
+            },
+            {
+                name: 'Portfolio',
+                href: '#Portfolio',
+            },
+            {
+                name: 'Contato',
+                href: '#Contact',
+            },
+        ]
+    },
+})
