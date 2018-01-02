fbc19bfe68b192b6416e725edf6cc1f552855dd5
PuckmanXY <kayoanderson0403@gmail.com>
2018-01-02 05:15:50 -0300

    VueJS ready to implement Portfolio section

--- js/vue.js
+++ js/vue.js
@@ -1,5 +1,5 @@
-var app = new Vue({
-    el: '#app',
+var navbarVue = new Vue({
+    el: '#navbarVue',
     data: {
         sections : [
             {
@@ -14,6 +14,45 @@ var app = new Vue({
                 name: 'Contato',
                 href: '#Contact',
             },
-        ]
+        ],
     },
+})
+
+var aboutSectionVue = new Vue({
+    el: '#About',
+    data: {
+        title: 'Quem sou eu?',
+        description: 'Olá, me chamo Kayo e tenho 17 anos. Sou desenvolvedor iniciante atualmente cursando Engenharia de Computação no Instituto Federal de Educação, Ciência e Tecnologia do Ceará e buscando novas chances de aprender novas coisas! Além disso também sou fotógrafo amador, músico para mim mesmo e entusiasta da poesia beat!'
+    },
+})
+
+var portfolioSectionVue = new Vue({
+    el: '#Portfolio',
+    data: {
+        projects: [
+            {
+                name: 'Lucas Silveira Webpage',
+                iconURL: 'img/Lucas_Silveira.png',
+                description: 'Minha primeira página web utilizando o framework Bootstrap',
+            },
+
+            {
+                name: 'Pursuit Game',
+                iconURL: 'img/Pursuit_Game.png',
+                description: 'Jogo feito em Python utilizando a biblioteca Turtle',
+            },
+
+            {
+                name: 'Fighting Beasts',
+                iconURL: 'img/Fighting_Beasts.png',
+                description: 'Jogo de texto com elementos rpg em Python utilizando a ferramenta TKinter',
+            },
+
+            {
+                name: 'Projeto Alma',
+                iconURL: 'img/Projeto_Alma.png',
+                description: '>Meu primeiro projeto fotográfico no Flickr',
+            },
+        ]
+    }
 })
